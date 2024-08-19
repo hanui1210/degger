76374241a91d1cb43b4db4d5d082b991719ecfa4
DESKTOP-5OBM98S\SEOULIT <hauiii1210@gmail.com>
2024-08-19 20:12:45 +0900

    표창날리기 최종

--- Dagger/Assets/scripts/DaggrController.cs
+++ Dagger/Assets/scripts/DaggrController.cs
@@ -6,6 +6,7 @@ using UnityEngine.UIElements;
 public class daggerContlloer : MonoBehaviour
 {
     private float speed = 0;
+    private float move = 0;
     
     void Start()
     {
@@ -20,31 +21,37 @@ public class daggerContlloer : MonoBehaviour
     }
 
     void Update()
-    { 
-       
-    // 표창스와이프해서 이동시키기 
-    //1. 화면을 누른다
-    //2. 드래그
-    //3. 화면에서 손을 뗀다
-    //4. 방향확인후 이동 
+    {
 
-    //스와이프 화면 감지 => 아래서 위로 
-    //bool isBottomDown = Input.GetMouseButtonDown(0); // 하단
-    //bool isToptDown = Input.GetMouseButtonDown(3); // 상단
-   
+        // 표창스와이프해서 이동시키기 
+        //1. 화면을 누른다
+        //2. 드래그
+        //3. 화면에서 손을 뗀다
+        //4. 방향확인후 이동 
+
+        //스와이프 화면 감지 => 아래서 위로 
+        //bool isBottomDown = Input.GetMouseButtonDown(0); // 하단
+        //bool isToptDown = Input.GetMouseButtonDown(3); // 상단
+        transform.position += new Vector3(0,move,0);
+        
+
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log(Input.mousePosition);
-            speed += 0.96f;
+         //   Debug.Log(Input.mousePosition);
+            speed =20;
+            move += 0.1f;
+           
 
         }
-        else if (Input.GetMouseButtonUp(3))
+        else if (Input.GetMouseButtonUp(0))
         {
+            speed =-20;
           //  speed += bottom;
-            Debug.Log(Input.mousePosition);
+          //  Debug.Log(Input.mousePosition);
         }
-        this.transform.Rotate(0, speed, 0);
-        Debug.Log(speed);
+            this.transform.Rotate(speed,0,0);
+        
+       Debug.Log(speed);
 
 
     }
